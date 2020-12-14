cd80406faef62f93a2c6ac6efe31e29023fae88b
Administrador <Administrador@172.17.99.160>
2020-12-14 15:05:53 -0500

    ACL en CuentaServiceImpl

--- MundoCostenio/src/main/java/co/com/mundocostenio/services/CuentaServiceImpl.java
+++ MundoCostenio/src/main/java/co/com/mundocostenio/services/CuentaServiceImpl.java
@@ -3,10 +3,16 @@ package co.com.mundocostenio.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import co.com.mundocostenio.domain.Cuenta;
 import co.com.mundocostenio.mybatis.mappers.CuentaMapper;
+import co.com.mundocostenio.security.acl.AccesControlListService;
 
 
 @Service("cuentaService")
@@ -14,26 +20,37 @@ public class CuentaServiceImpl implements CuentaService {
 	
 	@Autowired
 	private CuentaMapper cuentaMapper;
+	
+	@Autowired
+	private AccesControlListService<Cuenta> accesControlListService;
 
 	@Override
-	public Cuenta insert(Cuenta cuenta) {
+	@PreAuthorize(value ="hasRole('ROLE_COUNTER')")
+	@Transactional
+	public Cuenta insert(@Param("cuenta") Cuenta cuenta) {
 		this.cuentaMapper.insert(cuenta);
+		this.accesControlListService.insert(cuenta);
 		return cuenta;
 	}
 
 	@Override
-	public Cuenta update(Cuenta cuenta) {
+	@Transactional
+	@PreAuthorize(value="hasPermission(#cuenta, 'WRITE')")
+	public Cuenta update(@Param("cuenta") Cuenta cuenta) {
 		this.cuentaMapper.update(cuenta);
 		return cuenta;
 	}
 
 	@Override
-	public void delete(Cuenta cuenta) {
+	@Transactional
+	@PreAuthorize(value="hasPermission(#cuenta, 'DELETE')")
+	public void delete(@Param("cuenta") Cuenta cuenta) {
 		this.cuentaMapper.delete(cuenta);
 
 	}
 
 	@Override
+	@PostFilter("hasPermission(filterObject, 'READ')")
 	public List<Cuenta> select(Cuenta cuenta) {
 		return this.cuentaMapper.select(cuenta);
 	}
